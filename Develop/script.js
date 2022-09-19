75172de2292dd39b8148532e52e11beec85d47a3
Joseph Mogavero <joseph.mogavero@hotmail.com>
2022-09-19 14:34:58 -0400

    add alert user select at least one character type

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//function generate password, prompt user for password length 
+//function generate password, 
+//prompt user for password length 
 function generatePassword() {
   var password = "";
   var passwordLength = prompt("How many characters would you like your password to be? (8-128)");
@@ -9,12 +10,20 @@ function generatePassword() {
     alert("Password must be between 8 and 128 characters");
     return;
   }
-  
+
   //prompt user to choose lowercase, uppercase, numeric, and/or special characters
   var passwordLowercase = confirm("Would you like to include lowercase letters?");
   var passwordUppercase = confirm("Would you like to include uppercase letters?");
   var passwordNumeric = confirm("Would you like to include numbers?");
   var passwordSpecial = confirm("Would you like to include special characters?");
+
+  //if user does not select prompt for lowercase, uppercase, numeric, and/or special characters
+  //then alert user to select at least one character type
+  if (passwordLowercase === false && passwordUppercase === false && passwordNumeric === false && passwordSpecial === false) {
+  alert("You must select at least one character type");
+    return;
+  }
+
 }
 // Write password to the #password input
 function writePassword() {
