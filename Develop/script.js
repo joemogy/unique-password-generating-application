ae745597b09dfc5624be10ea54f72654baa100ce
Joseph Mogavero <joseph.mogavero@hotmail.com>
2022-09-19 14:24:54 -0400

    function generate password, prompt for lenght

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
+//function generate password, prompt user for password length 
+function generatePassword() {
+  var password = "";
+  var passwordLength = prompt("How many characters would you like your password to be? (8-128)");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8 and 128 characters");
+    return;
+  }
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
